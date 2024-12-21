14e133ce062759157a11692afa7c203f8217fcff
Dimitrios Gkegkas <dimitrisgegas01@gmail.com>
2024-12-21 22:16:04 +0900

    Added the scenarios for the baselines including the correct maps

--- scenarios/sumo/multi_scenario/38/scenario.py
+++ scenarios/sumo/multi_scenario/38/scenario.py
@@ -1,24 +1,37 @@
 import pickle
 from pathlib import Path
 from smarts.sstudio import gen_scenario
-from smarts.sstudio.types import (
+from smarts.sstudio.sstypes import (
+    MapSpec,
     Mission,
     Route,
     Scenario,
 )
 
-with open('routes.pkl', 'rb') as f:
+current_file_path = Path(__file__).resolve()
+with open(current_file_path.parent.parent / "routes.pkl", "rb") as f:
     # Load the content of the file using pickle.load()
     routes = pickle.load(f)
 
+assert routes, "routes.pkl is empty"
+
+# get the id based on the name of the parent folder as a number
+id = int(current_file_path.parent.name)
+assert routes[id], f"routes.pkl does not contain routes for scenario {id}"
+
 gen_scenario(
     scenario=Scenario(
+        map_spec=MapSpec(
+            source = str(current_file_path.parent.parent / "map.net.xml"),
+            shift_to_origin=False,
+            lanepoint_spacing=1.0,
+        ),
         traffic=None,
         ego_missions=[
-            Mission(Route(begin=("edge-east-EW", 0, 10), end=(routes[38][0], 0, 60))),
-            Mission(Route(begin=("edge-west-WE", 0, 10), end=(routes[38][1], 0, 60))),
-            Mission(Route(begin=("edge-north-NS", 0, 10), end=(routes[38][2], 0, 60))),
-            Mission(Route(begin=("edge-south-SN", 0, 10), end=(routes[38][3], 0, 60))),
+            Mission(Route(begin=("edge-east-EW", 0, 10), end=(routes[id][0], 0, 60))),
+            Mission(Route(begin=("edge-west-WE", 0, 10), end=(routes[id][1], 0, 60))),
+            Mission(Route(begin=("edge-north-NS", 0, 10), end=(routes[id][2], 0, 60))),
+            Mission(Route(begin=("edge-south-SN", 0, 10), end=(routes[id][3], 0, 60))),
         ],
     ),
     output_dir=Path(__file__).parent,
---
+++ scenarios/sumo/traffic_lights/FTTL1/43/scenario.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+from smarts.sstudio import gen_scenario
+from smarts.sstudio.sstypes import Scenario, Traffic, Flow, Route, RandomRoute, TrafficActor, Distribution, MapSpec
+import pickle
+
+
+traffic_actor = TrafficActor(
+    name="car",
+)
+
+current_file_path = Path(__file__).resolve()
+
+with open( str(current_file_path.parent.parent.parent / "routes.pkl"), "rb") as f:
+    # Load the content of the file using pickle.load()
+    routes = pickle.load(f)
+assert routes, "routes.pkl is empty"
+
+# Define the map and traffic
+map_spec = MapSpec(source= str(current_file_path.parent / "map.net.xml"))
+assert map_spec, "Map not found"
+
+# get the id based on the name of the parent folder as a number
+id = int(current_file_path.parent.name)
+assert routes[id], f"routes.pkl does not contain routes for scenario {id}"
+
+traffic = Traffic(flows=[
+    Flow(
+        route=Route(begin=("edge-east-EW", 0, 10), end=(routes[id][0], 0, 60)),
+            begin=0,
+            end=10 * 60 * 60,  # Flow lasts for 10 hours.
+            rate=1,  # One vehicle enters per second.
+            actors={traffic_actor: 1},
+    ),
+    Flow(
+        route=Route(begin=("edge-west-WE", 0, 10), end=(routes[id][1], 0, 60)),
+            begin=0,
+            end=10 * 60 * 60,  # Flow lasts for 10 hours.
+            rate=1,  # One vehicle enters per second.
+            actors={traffic_actor: 1},
+    ),
+    Flow(
+        route=Route(begin=("edge-north-NS", 0, 10), end=(routes[id][2], 0, 60)),
+            begin=0,
+            end=10 * 60 * 60,  # Flow lasts for 10 hours.
+            rate=1,  # One vehicle enters per second.
+            actors={traffic_actor: 1},
+    ),
+    Flow(
+        route=Route(begin=("edge-south-SN", 0, 10), end=(routes[id][3], 0, 60)),
+            begin=0,
+            end=10 * 60 * 60,  # Flow lasts for 10 hours.
+            rate=1,  # One vehicle enters per second.
+            actors={traffic_actor: 1},
+    ),
+])
+
+# Generate the scenario
+
+gen_scenario(
+    scenario=Scenario(
+        traffic={"basic": traffic},
+        map_spec=map_spec,  
+    ), 
+    output_dir= str(current_file_path.parent),
+)
