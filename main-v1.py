e47aae0dbe685ccbdb8f40c1e7110af245bb1a80
Dimitrios Gkegkas <dimitrisgegas01@gmail.com>
2025-01-04 19:22:51 +0900

    New training

--- main-v1.py
+++ main-v1.py
@@ -4,6 +4,13 @@ from smarts.zoo.agent_spec import AgentSpec
 from train.MultiAgentTrainer import MultiAgentTrainer
 
 
+class MultiAgentTrainer_v1 (MultiAgentTrainer):
+    def format_action(self, action):
+        if action:
+            return (0, 0)
+        else:
+            return (54, 0)
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -12,10 +19,10 @@ if __name__ == '__main__':
     parser.add_argument('--load_checkpoint', action='store_true', help='Load saved models')
     args = parser.parse_args()
 
-    trainer = MultiAgentTrainer(args)
+    trainer = MultiAgentTrainer_v1(args, algorithm_identifier='DuelingDDQNAgents-v1')
     trainer.initialize_environment(
         AgentSpec(
-            interface=AgentInterface.from_type(AgentType.Laner, max_episode_steps=None, top_down_rgb=True),
+            interface=AgentInterface.from_type(AgentType.LanerWithSpeed, max_episode_steps=None, top_down_rgb=True),
         )
     )
     trainer.initialize_agents()
