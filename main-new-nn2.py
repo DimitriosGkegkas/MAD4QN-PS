35b24be46e190dfd4c3b48a69b15c68ad6fc82f4
Dimitrios Gkegkas <dimitrisgegas01@gmail.com>
2025-02-24 10:42:40 +0900

    merged

--- main-new-nn2.py
+++ main-new-nn2.py
@@ -14,17 +14,18 @@ if __name__ == '__main__':
     args.headless = True
     
 
-    trainer = MultiAgentTrainerParallel(args, num_env=9, algorithm_identifier='DropOutLayer2',evaluation_step=10, evaluation=True)
+    trainer = MultiAgentTrainerParallel(args, num_env=9, algorithm_identifier='CheckT',evaluation_step=10, evaluation=True)
     trainer.initialize_environment(
         AgentSpec(
             interface=AgentInterface.from_type(AgentType.Laner, max_episode_steps=None, top_down_rgb=True),
         ),
     )
     trainer.initialize_agents(
-        Tmax=0.1,
+        Tmax=1,
         Tmin=0.01,
-        epsilon_decay_cycle_length = 1e5,
+        epsilon_decay_cycle_length = 1e4,
         replace=1e3,
         batch_size=2*256,
     )
+
     trainer.train()
