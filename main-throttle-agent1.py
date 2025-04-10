84b283f2b167022ace4013fd46afd2583f38334a
Dimitrios Gkegkas <dimitrisgegas01@gmail.com>
2025-04-10 17:28:04 +0200

    3 manauves in every cycle

--- main-throttle-agent1.py
+++ main-throttle-agent1.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     args.headless = False
     
 
-    trainer = MultiAgentTrainerThrottle(args, num_env=1, algorithm_identifier='maneuver', evaluation_step=10, agent_count = 1, evaluation=True)
+    trainer = MultiAgentTrainerThrottle(args, num_env=3, algorithm_identifier='maneuver', evaluation_step=10, agent_count = 1, evaluation=True)
     trainer.initialize_environment(
         AgentSpec(
             interface=AgentInterface(
