aff11b6f70386c3a30af7a8bd6b47b177f982f0c
Dimitrios Gkegkas <dimitrisgegas01@gmail.com>
2025-06-16 19:47:32 +0900

    Test file

--- test.py
+++ test.py
@@ -4,28 +4,28 @@ from train.MultiAgentTrainer import MultiAgentTrainerParallel, TrainerConfig
 
 if __name__ == '__main__':
     config = TrainerConfig(
-        algorithm_identifier="test",
+        algorithm_identifier="SAC2",
         num_env=2,
-        evaluation_step= 4,
-        max_training_steps= 100, # Maximum steps per episode, can be adjusted based on the environment
-        max_evaluation_steps=100,
+        evaluation_step=10,
+        max_training_steps=500, # Maximum steps per episode, can be adjusted based on the environment
+        max_evaluation_steps=500,
         
         scenario_subdir = "scenarios/sumo/multi_scenario_part",
         observation_shape=(128, 128, 3),  # Shape of the observation space, can be adjusted based on the environment
         
-        parallel=False,
-        envision=True,
-        evaluate=True
+        # parallel=False,
+        # envision=True,
+        # evaluate=True
     )
     agent_config = AgentConfig(
-        feature_dim=12,
+        feature_dim=64,
         message_dim=4,
         batch_size= 256, 
         
-        communication_hidden_dim= [10, 6],
-        critic_hidden_dim= [32, 64, 32, 16],
-        actor_hidden_dim= [32, 64, 32, 16, 4],
-        memory_max_size= int(500),
+        communication_hidden_dim= [32, 16],
+        critic_hidden_dim= [128, 128, 32],
+        actor_hidden_dim= [128, 128, 32],
+        memory_max_size= int(1000),
         
         reconstruction_coef=0.1,
         img_reconstruction_coef=0.1,
@@ -34,9 +34,9 @@ if __name__ == '__main__':
     )
 
     trainer = MultiAgentTrainerParallel(config, agent_config)
-    trainer.preload("models/agent_checkpoint.pth")
+    # trainer.preload("models/agent_checkpoint.pth")
     
 
-    # trainer.train()
-    trainer.envision(1)
+    trainer.train()
+    # trainer.envision(1)
     
